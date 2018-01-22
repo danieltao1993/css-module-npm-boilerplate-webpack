5216de03cf050e9597e80605040f8f8ffa307753
kongwentao <kongwentao@baidu.com>
2018-01-22 13:22:14 +0800

    before modify loader

--- example/src/index.js
+++ example/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Demo from './components/Demo';
+import Test from './components/Test';
 
 function App(props) {
     return (
         <main>
             <Demo />
+            <hr/>
+            <Test />
         </main>        
     );
 }
